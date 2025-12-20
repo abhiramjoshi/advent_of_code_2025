3f6ad580a6e20d95d070b7a3673ed8d0338a6968
abhiramjoshi <joshiabhiramj@gmail.com>
2025-12-20 00:46:18 -0500

    finishing day2

--- solutions/day2/puzzle2.py
+++ solutions/day2/puzzle2.py
@@ -11,18 +11,27 @@ with open(INPUT_FILE, "r") as f:
 print(ranges)
 
 s = 0
+invalids = set()
 for r in ranges:
     start, end = int(r[0]), int(r[1]) + 1
 
     for i in range(start, end):
         sr = str(i)
         n = len(sr) // 2
-        for j in range(n):
+        for j in range(1, n + 1):
             if len(sr) % j != 0:
                 continue
 
-            if sr[:n] == sr[n:]:
-                print("Invalid ID:", i)
-                s += i
+            valid = False
+            for k in range(1, len(sr) // j):
+                if sr[:j] != sr[j * k : j * (k + 1)]:
+                    valid = True
+                    break
+
+            if not valid:
+                if i not in invalids:
+                    print("Invalid ID:", i)
+                    s += i
+                    invalids.add(i)
 
 print("Final sum is:", s)
