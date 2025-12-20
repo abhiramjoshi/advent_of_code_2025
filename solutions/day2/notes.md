20215baaa6e81be3e4eb155965d5d96d2fac1b33
Abhi Joshi <joshi.abhirma98@gmail.com>
2025-12-20 04:37:04 +0000

    day2

---
+++ solutions/day2/notes.md
@@ -0,0 +1,5 @@
+Invalid ids can only have an even number of digits.
+
+Also since invalid IDs have repeated digits, we can loop through numbers based on half of the digits changing
+
+For each range, we must find 
---
+++ solutions/day2/puzzle1.py
@@ -0,0 +1,27 @@
+import sys
+
+INPUT_FILE = sys.argv[1]
+
+ranges = []
+with open(INPUT_FILE, "r") as f:
+    ranges = [
+        [x.split("-")[0], x.split("-")[1]] for x in f.read().strip("\n").split(",")
+    ]
+
+print(ranges)
+
+s = 0
+for r in ranges:
+    start, end = int(r[0]), int(r[1]) + 1
+
+    for i in range(start, end):
+        sr = str(i)
+        if len(sr) % 2 != 0:
+            continue
+
+        n = len(sr) // 2
+        if sr[:n] == sr[n:]:
+            print("Invalid ID:", i)
+            s += i
+
+print("Final sum is:", s)
